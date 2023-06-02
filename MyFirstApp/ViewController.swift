ada4172ef0beb47bfffcdd7ff7645a94231ea1c2
raynelz <97597336+zaharichswiftly@users.noreply.github.com>
2023-06-02 23:40:01 +0300

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Захар Литвинчук on 02.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
